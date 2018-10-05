3d878c9c196410f3bcbd7d19833b89aa822a349c
John Desrosiers <john.desro@gmail.com>
2018-10-04 18:55:20 -0700

    feat: Connect firebase app
    The firebase app is initialized when src/index.js is run.
    Create config file which exports an object containing the firebase app
    config data.
    
    The app will connect to Firebase whenever it is started.
    
    - Attempting to run src/index.js with jest-dom will throw an error.
      Do not put code that must be unit tested by Jest in the file.

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+  firebase: {
+    apiKey: 'AIzaSyA83ZW7JTfQ0Nv8ro4fRux_kQ3AfMufvOA',
+    authDomain: 'fb-cra-demo.firebaseapp.com',
+    databaseURL: 'https://fb-cra-demo.firebaseio.com',
+    projectId: 'fb-cra-demo',
+    storageBucket: 'fb-cra-demo.appspot.com',
+    messagingSenderId: '68444895745'
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
+import config from './config';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+firebase.initializeApp(config.firebase);
